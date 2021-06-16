290c49ae04f60b60123042eadb790321cb1237a5
Bob Li <60502081+BobLiSwigger@users.noreply.github.com>
2021-06-16 19:36:55 +0800

    fix a bug

--- src/opt.py
+++ src/opt.py
@@ -6,9 +6,9 @@
 def bp(W, b, params, alpha):
     y_res = params[0] # 标准输出 numpy.array[3]
     y_p = params[1] # 预测输出 numpy.array[3]
-    a = params[2] # 全连接层输入 numpy.array[144]
-    c = params[3] # sigmoid激活函数输出 numpy.array[576]
-    u = params[4] # 卷积层的输入矩阵 numpy.array[576, 25]
+    a = params[2] # 全连接层输入 numpy.array[3, 144]
+    c = params[3] # sigmoid激活函数输出 numpy.array[3, 576]
+    u = params[4] # 卷积层的输入矩阵 numpy.array[3, 576, 25]
     n = params[5] # 池化层输出的宽或高
     # 梯度下降
     for i in range(len(W[0].shape[0])):
@@ -17,7 +17,7 @@ def bp(W, b, params, alpha):
         b[1][i] -= t
         # 全连接层参数-权值w
         for j in range(len(W[1].shape[1])):
-            W[1][i, j] -= t*a[j]
+            W[1][i, j] -= t*a[i, j]
         # 卷积层参数-偏置b
         total = 0
         for j in range(len(W[1].shape[1])):
@@ -27,10 +27,10 @@ def bp(W, b, params, alpha):
             l2 = 2*w+1 + 2*h*2*n
             l3 = 2*w + (2*h+1)*2*n
             l4 = 2*w+1 + (2*h+1)*2*n
-            total += c[l1](1-c[l1])
-            total += c[l2](1-c[l2])
-            total += c[l3](1-c[l3])
-            total += c[l4](1-c[l4])
+            total += c[i, l1](1-c[i, l1])
+            total += c[i, l2](1-c[i, l2])
+            total += c[i, l3](1-c[i, l3])
+            total += c[i, l4](1-c[i, l4])
         b[0][i] -= t * W[1][i, j] * total
         # 卷积层参数-权值w
         for k in range(len(W[0].shape[1])):
@@ -42,9 +42,9 @@ def bp(W, b, params, alpha):
                 l2 = 2*w+1 + 2*h*2*n
                 l3 = 2*w + (2*h+1)*2*n
                 l4 = 2*w+1 + (2*h+1)*2*n
-                total += c[l1](1-c[l1]) * u[l1, k]
-                total += c[l2](1-c[l2]) * u[l2, k]
-                total += c[l3](1-c[l3]) * u[l3, k]
-                total += c[l4](1-c[l4]) * u[l4, k]
+                total += c[i, l1](1-c[i, l1]) * u[i, l1, k]
+                total += c[i, l2](1-c[i, l2]) * u[i, l2, k]
+                total += c[i, l3](1-c[i, l3]) * u[i, l3, k]
+                total += c[i, l4](1-c[i, l4]) * u[i, l4, k]
             W[0][i, k] -= t * W[1][i, j] * total
     return W, b
